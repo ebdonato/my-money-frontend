9485c46155f00b480875a0ed6d5004271954e441
Eduardo Donato <eduardo.donato@gmail.com>
2021-12-06 15:54:49 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Começando em React
+
+Aplicação Frontend criada como projeto exercício Ciclos de Pagamento do Curso [Curso React + Redux: Fundamentos e 2 Apps do Absoluto ZERO!](https://www.udemy.com/course/react-redux-pt/) da [Cod3r](https://www.cod3r.com.br)
+
+# Principais tecnologias usadas
+
+-   React/Redux
---
+++ src/common/template/footer.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default (props) => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2021
+            <a href="http://dev.page/ebdonato" target="_blank">
+                {" "}
+                ebdonato
+            </a>
+            {" | "}
+            <a href="http://cod3r.com.br" target="_blank">
+                {" "}
+                Cod3r
+            </a>
+        </strong>
+    </footer>
+)
